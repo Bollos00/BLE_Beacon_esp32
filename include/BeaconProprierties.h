1f74c46b6611a08dd80091b8fca5b44810e68e66
Bollos00 <bollos@outlook.com.br>
2022-11-19 21:02:36 -0300

    default rssi_values

--- include/BeaconProprierties.h
+++ include/BeaconProprierties.h
@@ -29,9 +29,9 @@
 // #define BEACON_SPEC BEACON_USE_IBEACON
 
 // Total period of beacon
-#define BEACON_ADVERTISING_TOTAL_PERIOD_S .2
+#define BEACON_ADVERTISING_TOTAL_PERIOD_S .9
 // Absolute value of the duty cycle
-#define BEACON_ADVERTISING_DUTY_CYCLE .8
+#define BEACON_ADVERTISING_DUTY_CYCLE .12
 
 // DO NO EDIT HERE!
 const uint32_t BEACON_ADVERTISING_TIME_MS  = (uint32_t)
@@ -42,13 +42,14 @@ const uint64_t BEACON_DEEP_SLEEP_TIME_US = (uint64_t)
 
 // Tx power in dBm(power of transmission)
 // Must be -12, -9, -6, -3, 0, +3, +6 or +9. Default will be 0 dBm
-#define BEACON_TX_POWER         -3
+#define BEACON_TX_POWER         -6
 
 // Propriertis of iBeacon and AltBeacon
 #define BEACON_UUID             "bc3411a4-3198-45c0-aefe-3f4985a0400f"
-#define BEACON_MAJOR            4005
-#define BEACON_MINOR            26
-#define BEACON_REF_RSSI         -60
+#define BEACON_MAJOR            11000
+#define BEACON_MINOR            1
+// Keep it undefined to use the default value
+// #define BEACON_REF_RSSI         -60
 
 // Extra proprierties of AltBeacon
 #if BEACON_SPEC == BEACON_USE_ALTBEACON
@@ -58,20 +59,44 @@ const uint64_t BEACON_DEEP_SLEEP_TIME_US = (uint64_t)
 
 // DO NO EDIT HERE!
 #if BEACON_TX_POWER == -12
-#define BEACON_TX_LEVEL ESP_PWR_LVL_N12
+    #define BEACON_TX_LEVEL ESP_PWR_LVL_N12
+    #ifndef BEACON_REF_RSSI
+        #define BEACON_REF_RSSI -67
+    #endif
 #elif BEACON_TX_POWER == -9
-#define BEACON_TX_LEVEL ESP_PWR_LVL_N9
+    #define BEACON_TX_LEVEL ESP_PWR_LVL_N9
+    #ifndef BEACON_REF_RSSI
+        #define BEACON_REF_RSSI -63
+    #endif
 #elif BEACON_TX_POWER == -6
-#define BEACON_TX_LEVEL ESP_PWR_LVL_N6
+    #define BEACON_TX_LEVEL ESP_PWR_LVL_N6
+    #ifndef BEACON_REF_RSSI
+        #define BEACON_REF_RSSI -58
+    #endif
 #elif BEACON_TX_POWER == -3
-#define BEACON_TX_LEVEL ESP_PWR_LVL_N3
+    #define BEACON_TX_LEVEL ESP_PWR_LVL_N3
+    #ifndef BEACON_REF_RSSI
+        #define BEACON_REF_RSSI -54
+    #endif
 #elif BEACON_TX_POWER == +3
-#define BEACON_TX_LEVEL ESP_PWR_LVL_P3
+    #define BEACON_TX_LEVEL ESP_PWR_LVL_P3
+    #ifndef BEACON_REF_RSSI
+        #define BEACON_REF_RSSI -45
+    #endif
 #elif BEACON_TX_POWER == +6
-#define BEACON_TX_LEVEL ESP_PWR_LVL_P6
+    #define BEACON_TX_LEVEL ESP_PWR_LVL_P6
+    #ifndef BEACON_REF_RSSI
+        #define BEACON_REF_RSSI -41
+    #endif
 #elif BEACON_TX_POWER == +9
-#define BEACON_TX_LEVEL ESP_PWR_LVL_P9
+    #define BEACON_TX_LEVEL ESP_PWR_LVL_P9
+    #ifndef BEACON_REF_RSSI
+        #define BEACON_REF_RSSI -37
+    #endif
 #else
-#define BEACON_TX_LEVEL ESP_PWR_LVL_N0
+    #define BEACON_TX_LEVEL ESP_PWR_LVL_N0
+    #ifndef BEACON_REF_RSSI
+        #define BEACON_REF_RSSI -50
+    #endif
 #endif
 // DO NO EDIT HERE!
